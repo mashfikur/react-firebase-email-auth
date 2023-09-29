e3b2aade98b7e666bc10f8b21d5d6e52739fae0f
mashfikur <120697837+mashfikur@users.noreply.github.com>
2023-09-29 13:09:58 +0600

    firebase configeration setup

--- src/components/HeroRegister.jsx
+++ src/components/HeroRegister.jsx
@@ -14,7 +14,7 @@ const HeroRegister = () => {
       <div className="hero ">
         <div className="hero-content flex-col lg:flex-row-reverse gap-10">
           <div className="text-center lg:text-left">
-            <h1 className="text-5xl font-bold">Login now!</h1>
+            <h1 className="text-5xl font-bold">Register now!</h1>
             <p className="py-6">
               Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
               excepturi exercitationem quasi. In deleniti eaque aut repudiandae
@@ -52,7 +52,7 @@ const HeroRegister = () => {
                   </label>
                 </div>
                 <div className="form-control mt-6">
-                  <button className="btn btn-primary">Login</button>
+                  <button className="btn btn-primary">Register</button>
                 </div>
               </form>
             </div>
---
+++ src/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA4snbN31lBlLlBmfV4vKWpcSP-gc2iAlI",
+  authDomain: "react-firebase-email-aut-84536.firebaseapp.com",
+  projectId: "react-firebase-email-aut-84536",
+  storageBucket: "react-firebase-email-aut-84536.appspot.com",
+  messagingSenderId: "1070154345964",
+  appId: "1:1070154345964:web:b87435d9384607f8bce4c9",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
